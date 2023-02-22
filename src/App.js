392b25cf2f8c6d3f0c43a06bbd07634139bac757
pctinkychausky <pctinkychausky@gmail.com>
2023-02-22 01:24:01 +0000

    temp chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LineChart from "./components/LineChart";
+import { useState, useEffect } from "react";
+import { Data } from "./utils/Data";
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
 
 function App() {
+  Chart.register(CategoryScale);
+  const [ApiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=42d08d24aa2287eeb9badbbd8c4cac72&units=metric`;
+    const fetchData = async () => {
+      try {
+        let response = await fetch(url);
+        if (response.status === 200) {
+          let data = await response.json();
+          setApiData(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const tempArray = ApiData?.list.map((data) => data.main.temp.toFixed(2));
+
+  const shortenTempArray = tempArray.slice(0, 5);
+
+  const numberTempArray = shortenTempArray?.map(Number);
+
+  console.log("🚀 ~ file: App.js:33 ~ App ~ numberTempArray:", numberTempArray);
+
+  const [chartData, setChartData] = useState({
+    labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
+    datasets: [
+      {
+        label: "Users Gained ",
+
+        data: numberTempArray,
+
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart chartData={chartData} />
     </div>
   );
 }
---
+++ src/utils/Data.js
@@ -0,0 +1,27 @@
+export const Data = [
+  {
+    id: 1,
+    year: 2012,
+    userGain: 32,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 30.44,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 25.2,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 15,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 2,
+  },
+];
